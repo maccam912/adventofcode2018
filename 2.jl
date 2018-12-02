f2a8bf11dd1c333c3a6e65878855641f6b1d2e4c
U-DESKTOP-RHS9FAU\macca <maccam912@gmail.com>
2018-12-02 10:44:45 -0600

    day 2

--- 2.jl
+++ 2.jl
@@ -1,6 +1,6 @@
 include("util.jl")
 
-inputs = getlines("inputs/2_test.txt")
+inputs = getlines("inputs/2.txt")
 
 function string_to_dict(i::String)::Dict{Char,Int64}
     retval = Dict()
@@ -32,6 +32,34 @@ function has_three(i::String)::Bool
     return has_n(i, 3)
 end
 
+function compare_strings(a::String, b::String)::Tuple{Int64,Array{Char}}
+    in_common = []
+    difference = 0
+    for i in 1:length(a)
+        if a[i] == b[i]
+            push!(in_common, a[i])
+        else
+            difference += 1
+        end
+    end
+    return (difference, in_common)
+end
+
+function get_pairs(inputs)
+    pairlist::Array{Tuple{String,String}} = []
+    for i in 1:(length(inputs)-1)
+        for j in (i+1):length(inputs)
+            push!(pairlist, (inputs[i],inputs[j]))
+        end
+    end
+    return pairlist
+end
+
+
 twos = filter(has_two, inputs)
 threes = filter(has_three, inputs)
 println(length(twos)*length(threes))
+
+difflist = map(x -> compare_strings(x...), get_pairs(inputs))
+diff_of_one = filter(x -> x[1] == 1, difflist)
+println(join(diff_of_one[1][2]))
